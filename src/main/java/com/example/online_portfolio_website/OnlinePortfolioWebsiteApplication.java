d4b2d93d97da28a6213c0114b034688f068380a2
tayjes <tayjes2005@gmail.com>
2026-07-03 13:04:00 +0530

    Initial Project

---
+++ src/main/java/com/example/online_portfolio_website/OnlinePortfolioWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_portfolio_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePortfolioWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePortfolioWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online_portfolio_website/OnlinePortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_portfolio_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
